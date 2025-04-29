6726cac3028814d65b34a9fb9627042fb5a0bc14
Harippriya S <harippriyasiva@gmail.com>
2025-04-29 17:11:59 +0530

    Create Boolean_if..py

---
+++ Python basics/Boolean_if..py
@@ -0,0 +1,12 @@
+light_is_on = True
+
+if light_is_on:
+  print("The light is on.")
+  print("It's bright in here.")
+print("The program is done.")
+
+light_is_on = False
+
+if light_is_on:
+  print("OMG.")
+  print("Super bright.")
